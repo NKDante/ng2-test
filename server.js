e5488e341dce9ca7d449249c1bf99b0a348197f4
Nikitka <kovalevnv@altarix.ru>
2017-12-21 07:12:25 +0400

    fix

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function () {
+  console.log("Listening on Port 3000");
+});
